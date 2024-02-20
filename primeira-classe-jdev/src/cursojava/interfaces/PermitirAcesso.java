20e4869c6feadc71c2204d00c7fc8aa9be1b88e6
Kennedy Vieira <kennedy.psic@gmail.com>
2024-02-19 21:44:31 -0300

    adicionando pacote de Threads

--- primeira-classe-jdev/src/cursojava/interfaces/PermitirAcesso.java
+++
@@ -1,8 +0,0 @@
-package cursojava.interfaces;
-
-public interface PermitirAcesso {
-	
-	public boolean autenticar();
-	public boolean autenticar(String login, String senha);
-
-}
---
+++ primeiro-java-eclipse/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,14 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	public static void main(String[] args) {
+	
+
+	//FOR é usado quando é necessário percorrer em quantidade de vezes números, listas, dados
+	
+		for(int numero = 10; numero <=0; numero--) {
+			System.out.println("Número atual é: " + numero);
+		}
+		
+	}
+}
