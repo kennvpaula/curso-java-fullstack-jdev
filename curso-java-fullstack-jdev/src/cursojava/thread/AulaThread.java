bbfcb5e2fe88ef2b4745e86c16b924816fa30f37
Kennedy Vieira <kennedy.psic@gmail.com>
2024-02-19 22:50:27 -0300

    Criando a primeira Thread

--- curso-java-fullstack-jdev/src/cursojava/thread/AulaThread.java
+++ curso-java-fullstack-jdev/src/cursojava/thread/AulaThread.java
@@ -1,15 +1,36 @@
 package cursojava.thread;
 
+import java.awt.JobAttributes;
+
+import javax.swing.JOptionPane;
+
 public class AulaThread {
 	public static void main(String[] args) throws InterruptedException {
-		for (int pos = 0; pos < 10; pos++) {
+		
+		//Thread processando em paralelo
+		new Thread() {
 
-			Thread.sleep(1000);
-			// Quero executar esse envio com tempo de parada ou com tempo determinado
-			System.out.println("Executando rotina de envio de e-mail");
+			public void run() {
+				// código da rotina que executa em paralelo
+				for (int pos = 0; pos < 10; pos++) {
+					// Quero executar esse envio com tempo de parada ou com tempo determinado
+					System.out.println("Executando rotina de envio de e-mail");
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
 
-		}
+				}
+				// Fim do código em paralelo
 
+			};
+
+		}.start(); // liga a Thread que fica processando paralelamente por trás
+
+		// Código do sistema do usuário continua fluxo de trabalho
 		System.out.println("Chegou o fim do código de teste da Thread");
+		// Fluxo do sistema, cadastro de venda, emissão de NF
+		JOptionPane.showMessageDialog(null, "Sistema continua executando para o usuário");
 	}
 }
